caa3ca4f4fa924e4c1bcf610c637ab7b20890402
2025246 <rmansfield@creightonprep.org>
2024-12-28 22:38:13 -0600

    defualt values

--- TeamCode/src/main/java/pedroPathing/constants/LConstants.java
+++ TeamCode/src/main/java/pedroPathing/constants/LConstants.java
@@ -1,11 +1,24 @@
 package pedroPathing.constants;
 
+import com.pedropathing.localization.Encoder;
 import com.pedropathing.localization.constants.OTOSConstants;
+import com.pedropathing.localization.constants.ThreeWheelConstants;
 import com.qualcomm.hardware.sparkfun.SparkFunOTOS;
 
 public class LConstants {
     static {
-        OTOSConstants.offset = new SparkFunOTOS.Pose2D(3,2,Math.toRadians(270));
+        ThreeWheelConstants.forwardTicksToInches = .001989436789;
+        ThreeWheelConstants.strafeTicksToInches = .001989436789;
+        ThreeWheelConstants.turnTicksToInches = .001989436789;
+        ThreeWheelConstants.leftY = 1;
+        ThreeWheelConstants.rightY = -1;
+        ThreeWheelConstants.strafeX = -2.5;
+        ThreeWheelConstants.leftEncoder_HardwareMapName = "leftFront";
+        ThreeWheelConstants.rightEncoder_HardwareMapName = "rightRear";
+        ThreeWheelConstants.strafeEncoder_HardwareMapName = "rightFront";
+        ThreeWheelConstants.leftEncoderDirection = Encoder.REVERSE;
+        ThreeWheelConstants.rightEncoderDirection = Encoder.REVERSE;
+        ThreeWheelConstants.strafeEncoderDirection = Encoder.FORWARD;
 
     }
 }
