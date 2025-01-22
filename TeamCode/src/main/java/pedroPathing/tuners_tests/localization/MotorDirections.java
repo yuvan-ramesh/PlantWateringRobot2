94181af9c96489a85d1bd25b07b9e5dffcd2c88f
BaronClaps <126834072+BaronClaps@users.noreply.github.com>
2025-01-22 06:41:31 -0600

    MotorDirections.java - dt motors should stop if not pressed

--- TeamCode/src/main/java/pedroPathing/tuners_tests/localization/MotorDirections.java
+++ TeamCode/src/main/java/pedroPathing/tuners_tests/localization/MotorDirections.java
@@ -76,15 +76,23 @@ public class MotorDirections extends OpMode {
 
         if(gamepad1.a)
             leftFront.setPower(1);
+        else
+            leftFront.setPower(0);
 
         if(gamepad1.y)
             leftRear.setPower(1);
+        else
+            leftRear.setPower(0);
 
         if(gamepad1.b)
             rightFront.setPower(1);
+        else
+            rightFront.setPower(0);
 
         if(gamepad1.x)
             rightRear.setPower(1);
+        else
+            rightRear.setPower(0);
 
         telemetryA.addLine("Press A to spin the left front motor at 100% power");
         telemetryA.addLine("Press Y to spin the left rear motor at 100% power");
